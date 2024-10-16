ba0af80bf34fadd813debe1db98a854e202f1d54
Muhammed Can <mucan54@hotmail.com>
2024-10-16 13:23:21 +0300

    RoundedRectangle model updates

--- packages/maker.js/src/models/RoundedRectangle.ts
+++ packages/maker.js/src/models/RoundedRectangle.ts
@@ -16,38 +16,38 @@ namespace MakerJs.models {
             this.paths = {};
 
             // Bottom-left corner (rounded or sharp)
-            if (tlRadius > 0) {
-                this.paths['bottomLeftArc'] = new paths.Arc([tlRadius, tlRadius], tlRadius, 180, 270);
+            if (blRadius > 0) {
+                this.paths['bottomLeftArc'] = new paths.Arc([blRadius, blRadius], blRadius, 180, 270);
             } else {
                 this.paths['bottomLeft'] = new paths.Line([0, 0], [0, 0]); // Sharp corner at (0, 0)
             }
 
             // Bottom-right corner (rounded or sharp)
-            if (trRadius > 0) {
-                this.paths['bottomRightArc'] = new paths.Arc([length - trRadius, tlRadius], trRadius, 270, 360);
+            if (brRadius > 0) {
+                this.paths['bottomRightArc'] = new paths.Arc([length - brRadius, brRadius], brRadius, 270, 360);
             } else {
                 this.paths['bottomRight'] = new paths.Line([length, 0], [length, 0]); // Sharp corner at (length, 0)
             }
 
             // Top-right corner (rounded or sharp)
-            if (brRadius > 0) {
-                this.paths['topRightArc'] = new paths.Arc([length - brRadius, width - brRadius], brRadius, 0, 90);
+            if (trRadius > 0) {
+                this.paths['topRightArc'] = new paths.Arc([length - trRadius, width - trRadius], trRadius, 0, 90);
             } else {
                 this.paths['topRight'] = new paths.Line([length, width], [length, width]); // Sharp corner at (length, width)
             }
 
             // Top-left corner (rounded or sharp)
-            if (blRadius > 0) {
-                this.paths['topLeftArc'] = new paths.Arc([blRadius, width - blRadius], blRadius, 90, 180);
+            if (tlRadius > 0) {
+                this.paths['topLeftArc'] = new paths.Arc([tlRadius, width - tlRadius], tlRadius, 90, 180);
             } else {
                 this.paths['topLeft'] = new paths.Line([0, width], [0, width]); // Sharp corner at (0, width)
             }
 
             // Define straight edges between corners
-            this.paths['bottomEdge'] = new paths.Line([tlRadius, 0], [length - trRadius, 0]);
-            this.paths['rightEdge'] = new paths.Line([length, tlRadius], [length, width - brRadius]);
-            this.paths['topEdge'] = new paths.Line([length - brRadius, width], [blRadius, width]);
-            this.paths['leftEdge'] = new paths.Line([0, width - blRadius], [0, tlRadius]);
+            this.paths['bottomEdge'] = new paths.Line([blRadius, 0], [length - brRadius, 0]);
+            this.paths['rightEdge'] = new paths.Line([length, brRadius], [length, width - trRadius]);
+            this.paths['topEdge'] = new paths.Line([length - trRadius, width], [tlRadius, width]);
+            this.paths['leftEdge'] = new paths.Line([0, width - tlRadius], [0, blRadius]);
         }
     }
 
@@ -55,9 +55,9 @@ namespace MakerJs.models {
     (<IKit>RoundedRectangle).metaParameters = [
         { title: "Length", type: "range", min: 1, max: 100, value: 50 },
         { title: "Width", type: "range", min: 1, max: 100, value: 50 },
-        { title: "Bottom-Left Radius", type: "range", min: 0, max: 20, value: 10 },
-        { title: "Bottom-Right Radius", type: "range", min: 0, max: 20, value: 10 },
+        { title: "Top-Left Radius", type: "range", min: 0, max: 20, value: 10 },
         { title: "Top-Right Radius", type: "range", min: 0, max: 20, value: 10 },
-        { title: "Top-Left Radius", type: "range", min: 0, max: 20, value: 10 }
+        { title: "Bottom-Right Radius", type: "range", min: 0, max: 20, value: 10 },
+        { title: "Bottom-Left Radius", type: "range", min: 0, max: 20, value: 10 }
     ];
 }
