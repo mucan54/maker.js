632f684f02113958856d6cf92bf4f616c3439fc8
Muhammed Can <mucan54@hotmail.com>
2024-10-29 15:50:16 +0300

    build strategy is updated

--- packages/maker.js/target/build.js
+++ packages/maker.js/target/build.js
@@ -2,32 +2,42 @@
 const fs = require('fs');
 const utf8 = 'utf8';
 
-//add versioned header to top of declaration file
+// Add versioned header to the top of the TypeScript declaration file
 const dts = './dist/index.d.ts';
-var tsdHeader = fs.readFileSync('./target/tsd-header.txt', utf8);
-var versionedHeader = tsdHeader.replace(/#VERSION#/, require('../package.json').version);
-var content = fs.readFileSync(dts, utf8);
+const tsdHeader = fs.readFileSync('./target/tsd-header.txt', utf8);
+const versionedHeader = tsdHeader.replace(/#VERSION#/, require('../package.json').version);
+const content = fs.readFileSync(dts, utf8);
 fs.writeFileSync(dts, versionedHeader + content, utf8);
 
-//add version code to bottom of js file
+// Add version code to the bottom of the JavaScript file
 const js = './dist/index.js';
 fs.appendFileSync(js, 'MakerJs.version = "' + require('../package.json').version + '";\n');
 
-//browserify
-const b = browserify();
-b.plugin('licensify');
+// Configure Browserify to expose makerjs as an AMD module
+const b = browserify(js, {
+    standalone: 'makerjs', // Expose makerjs as a UMD (Universal Module Definition) module
+    plugin: ['licensify']
+});
+
 b.ignore('buffer');
-b.require(js, { expose: 'makerjs' });
-b.bundle(function (err, buf) {
+b.bundle((err, buf) => {
+    if (err) {
+        console.error("Error during bundling:", err);
+        return;
+    }
+
+    // Read and add header content
     const header = fs.readFileSync('./target/header.txt', utf8);
     const browserCode = header + buf.toString(utf8);
-    fs.writeFileSync('./dist/browser.maker.js', browserCode, utf8);
 
-    //also copy to web
+    // Write the browser-compatible code to the target output files
+    fs.writeFileSync('./dist/browser.maker.js', browserCode, utf8);
     fs.writeFileSync('../../docs/target/js/browser.maker.js', browserCode, utf8);
 
-    //append header to node version
+    // Append the header to the node version
     const nodeCode = header + fs.readFileSync(js, utf8);
     const nodeFooter = fs.readFileSync('./target/node-requires.js', utf8);
     fs.writeFileSync(js, nodeCode + nodeFooter, utf8);
+
+    console.log("Maker.js built successfully for AMD compatibility.");
 });
