611d81cc838178372c1c38c8aaa6001abd003507
Muhammed Can <mucan54@hotmail.com>
2024-12-09 12:59:59 +0300

    layer is added to main model.

--- packages/maker.js/src/core/manager.ts
+++ packages/maker.js/src/core/manager.ts
@@ -130,6 +130,7 @@ namespace MakerJs.manager {
 
     export function fetchMainModel(name: string, parameters: any[], unitType = "cm"): any {
         let mainModel = getModel(name, parameters);
+        mainModel.layer = "black";
         mainModel.units = unitType ?? "cm";
         managerMainModel = mainModel;
         return mainModel;
